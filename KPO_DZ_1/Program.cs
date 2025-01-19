7d2a6523c316b4ade392884479f8f9eecd591a43
Diana Chade <91020940+ItsDianthus@users.noreply.github.com>
2025-01-19 23:47:27 +0300

    Update Program.cs

--- KPO_DZ_1/Program.cs
+++ KPO_DZ_1/Program.cs
@@ -10,31 +10,26 @@ namespace KPO_DZ_1
     {
         static void Main(string[] args)
         {
-            var customers = new List<Customer>
-            {
-            new() { Name = "Adam" },
-            new() { Name = "John" },
-            new() { Name = "Jack" },
-            };
-
+            var customers = new List<Customer> { new() { Name = "Adam" }, new() { Name = "John" }, new() { Name = "Jack" } };
             var factory = new FactoryAF(customers);
-
-            for (int i = 0; i < 6; i++)
-                factory.AddCar();
-
-            Console.WriteLine("At the start");
-
-            Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
-
-            Console.WriteLine(string.Join(Environment.NewLine, factory.Customers));
-
-            factory.SaleCar();
-
-            Console.WriteLine("After sale");
-
-            Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
-
-            Console.WriteLine(string.Join(Environment.NewLine, factory.Customers));
+        
+            for (int i = 0; i < 6; i++) factory.AddCar();
+        
+            Console.WriteLine("\n=== At the start ===");
+            Print(factory);
+        
+            foreach (var customer in customers)
+                if (factory.SaleCar()) Console.WriteLine($"Машина продана клиенту: {customer.Name}");
+        
+            Console.WriteLine("\n=== After sale ===");
+            Print(factory);
         }
+    
+        static void Print(FactoryAF factory)
+        {
+            Console.WriteLine("Автомобили: " + (factory.Cars.Any() ? string.Join(", ", factory.Cars) : "Нет"));
+            Console.WriteLine("Клиенты: " + (factory.Customers.Any() ? string.Join(", ", factory.Customers) : "Нет"));
+        }
+
     }
 }
